a281a681e9c71de41dc8f7646008ece355efb84c
Noturritik <122246719+Noturritik@users.noreply.github.com>
2023-01-12 12:07:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return 'LazyDeveloper'
 
-os.system("git clone https://Noturritik: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@github.com/Noturritik/MoViEs_xD1  okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://Noturritik: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@github.com/Noturritik/LazyPrincessV2  okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
